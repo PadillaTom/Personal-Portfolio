fb9cde3d0cc789c70c4e994af33ce95fe6da7eff
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-29 09:11:21 +0200

    Comenzamos Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,27 @@
 import React from "react"
-
+import { Link } from "gatsby"
 const Navbar = () => {
-  return <nav className="navbar-container">Hello from Nav</nav>
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo"></div>
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
